04b6d747d59cd086ae035b39c1fcbde3f81482c0
wharley <wharleyangelica@gmail.com>
2017-02-05 01:42:44 -0200

    speechToText

---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+declare let window: any;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public spokenWords : Array<string> = new Array<string>();
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  SpeechToText() {
+    window.plugins.speechrecognizer.startRecognize((result) =>{
+      this.spokenWords.push(result);
+    }, (errorMessage) => {
+      alert(errorMessage)
+    })
+  }
+
+}
